1c0206ec77e71d0244d35eeb7bc592b288ec9d69
David Lopes Gomes <gomes.david.9@gmail.com>
2023-01-12 21:21:35 -0300

    Add files via upload

--- controllers/CadastroController.js
+++ controllers/CadastroController.js
@@ -6,19 +6,25 @@ class CadastroController {
         this.password = password;
         this.password_confirmation = password_confirmation
         this.rg = rg
+        this.erro=['',
+                   'Senha e Confirmação diferentes', 
+                   'Senha tem menos de 8 caracteres', 
+                   'Senha tem mais de 20 caracteres']
     }
-    /*telephoneIsValid=function() {
-        let phoneRegex = /^[2-9]\d{2}[2-9]\d{2}\d{4}$/;
-        var digits = telephone.replace(/\D/g, "");
-        return phoneRegex.test(digits);
-        
-      }*/
     passwordIsValid = function (password, password_confirmation) {
-        if ((password === password_confirmation) && (password.length >= 8) && (password.length <= 20)) {
+        if ((password === password_confirmation) &&
+            (password.length >= 8) && 
+            (password.length <= 20)) {
             return true;
         }
-        else {
-            return false;
+        else if(password !== password_confirmation){
+            return this.erro[1];
+        }
+        else if(password.length < 8){
+            return this.erro[2];
+        }
+        else if(password.length > 20){
+            return this.erro[3];
         }
     }
 }
@@ -31,13 +37,14 @@ form.addEventListener('submit', (e) => {
     const cadastro = new Cadastro(document.getElementById('full_name').value, document.getElementById('email').value,
         document.getElementById('password').value, document.getElementById('password_confirmation').value,
         document.getElementById('rg').value)
-    if(cadastroController.passwordIsValid(cadastroController.password,cadastroController.password_confirmation) == true) {
+        let erro = cadastroController.passwordIsValid(cadastroController.password,cadastroController.password_confirmation)
+    if(erro == true) {
         cadastroController.array_cadastro.push(cadastro)
         alert("Você se cadastrou na lista de Espera");
     }
     else {
         console.log("Erro")
-        alert("Erro: Não foi possível a realização do cadastro na lista de Espera");
+        alert(erro);
         e.preventDefault();
     }
 });
